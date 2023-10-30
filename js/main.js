90e970d971eb3479a2ff5378dd530c783b0d332d
Marco <marcoregina92@gmail.com>
2023-10-30 17:28:51 +0100

    completa esercizio

--- js/main.js
+++ js/main.js
@@ -1,22 +1,38 @@
-const ratekm = 0.21
+const rateKm = 0.21;
 
 let nameInput = document.querySelector("#name");
 let kmInput = document.querySelector("#kilometres");
-let ageInput = document.querySelector("age");
-let buttonInput = document.querySelector("generates");
-let offerInput = document.querySelector("offer");
-let carriageInput = document.querySelector("carriage");
-let codexInpunt = document.querySelector("codex");
-let ticketInpunt = document.querySelector("ticket");
+let ageInput = document.querySelector("#age");
+let buttonInput = document.querySelector("#generates");
+let offerInput = document.querySelector("#offer");
+let carriageInput = document.querySelector("#carriage");
+let codexInput = document.querySelector("#codex");
+let ticketInput = document.querySelector("#ticket");
 
 buttonInput.addEventListener("click" , function() {
+
+    let randomCarriage = parseInt((Math.random() * 20) + 1);
+    let codex = parseInt((Math.random() * 10000));
 
     if (ageInput.value=="senior") {
         document.getElementById("passengerName").innerHTML = nameInput.value;
         offerInput.innerHTML = "Hai diritto ad uno sconto Over-65";
+        carriageInput.innerHTML = randomCarriage;
+        codexInput.innerHTML = codex;
+        ticketInput.innerHTML = ((Number(kmInput.value) * rateKm) * 0.6).toFixed(2) + " €";
+    } else if (ageInput.value=="underage") {
+        document.getElementById("passengerName").innerHTML = nameInput.value;
+        offerInput.innerHTML = "Hai diritto ad uno sconto Under-18";
+        carriageInput.innerHTML = randomCarriage;
+        codexInput.innerHTML = codex;
+        ticketInput.innerHTML = ((Number(kmInput.value) * rateKm) * 0.8).toFixed(2) + " €";
+    } else {
+        document.getElementById("passengerName").innerHTML = nameInput.value;
+        offerInput.innerHTML = "Biglietto normale";
+        carriageInput.innerHTML = randomCarriage;
+        codexInput.innerHTML = codex;
+        ticketInput.innerHTML = ((Number(kmInput.value) * rateKm)).toFixed(2) + " €";
     }
-
-
 });
 
 
