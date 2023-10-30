e05eaf1cf05f504223b598858f72e778cfd1c46a
Marco <marcoregina92@gmail.com>
2023-10-30 16:53:56 +0100

    problema

--- js/main.js
+++ js/main.js
@@ -1,10 +1,52 @@
- let nameInput = document.querySelector("#name");
- let kmInput = document.querySelector("#kilometres");
- let ageInput = document.querySelector("#age");
- let btn = document.querySelector("#generates");
+const ratekm = 0.21
 
- btn.addEventListener("click", function() {
-     console.log("L'utente ha scritto: " + nameInput.value);
-     console.log("L'utente ha scritto: " + kmInput.value);
-     console.log("L'utente ha scritto: " + ageInput.value);
- });
+let nameInput = document.querySelector("#name");
+let kmInput = document.querySelector("#kilometres");
+let ageInput = document.querySelector("age");
+let buttonInput = document.querySelector("generates");
+let offerInput = document.querySelector("offer");
+let carriageInput = document.querySelector("carriage");
+let codexInpunt = document.querySelector("codex");
+let ticketInpunt = document.querySelector("ticket");
+
+buttonInput.addEventListener("click" , function() {
+
+    if (ageInput.value=="senior") {
+        document.getElementById("passengerName").innerHTML = nameInput.value;
+        offerInput.innerHTML = "Hai diritto ad uno sconto Over-65";
+    }
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// btn.addEventListener("click", function() {
+//      console.log("L'utente ha scritto: " + nameInput.value);
+//      console.log("L'utente ha scritto: " + kmInput.value);
+//      console.log("L'utente ha scritto: " + ageInput.value);
+
+//      if (ageInput.value=="senior") {
+        
+//         console.log("Sei over65");
+//      } else if (ageInput.value=="underage") {
+//         console.log("Sei minorenne");
+//      } else {
+//         console.log("Sei adulto");
+//      }
+// });
